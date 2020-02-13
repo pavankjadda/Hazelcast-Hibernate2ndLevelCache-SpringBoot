7decaa07a5de83fc72314b4c4c8879b12bed23c4
Pavan Jadda <jpavanaryan@gmail.com>
2020-02-12 20:53:51 -0500

    first commit

---
+++ src/main/java/com/pj/hazelcastdemo/HazelcastDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pj.hazelcastdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastDemoApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HazelcastDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pj/hazelcastdemo/HazelcastDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pj.hazelcastdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastDemoApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
