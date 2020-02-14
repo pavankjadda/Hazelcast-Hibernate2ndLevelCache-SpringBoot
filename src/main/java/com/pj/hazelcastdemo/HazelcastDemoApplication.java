dcdc1e4ae37bee16fb29692c416a6f3077a61030
Pavan Jadda <jpavanaryan@gmail.com>
2020-02-13 22:31:21 -0500

    Add Hazelcast Java config

--- src/main/java/com/pj/hazelcastdemo/HazelcastDemoApplication.java
+++ src/main/java/com/pj/hazelcastdemo/HazelcastDemoApplication.java
@@ -1,7 +1,5 @@
 package com.pj.hazelcastdemo;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +8,6 @@ public class HazelcastDemoApplication
 {
 	public static void main(String[] args)
 	{
-		connectToHazelcastClient();
 		SpringApplication.run(HazelcastDemoApplication.class, args);
-	}
-
-	private static void connectToHazelcastClient()
-	{
-		ClientConfig clientConfig = new ClientConfig();
-		clientConfig.getNetworkConfig().addAddress("127.0.0.1");
-		HazelcastClient.newHazelcastClient(clientConfig);
 	}
 }
--- src/main/java/com/pj/hazelcastdemo/config/HazelCastConfig.java
+++ src/main/java/com/pj/hazelcastdemo/config/HazelCastConfig.java
@@ -1,16 +1,21 @@
 package com.pj.hazelcastdemo.config;
 
-import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HazelCastConfig
 {
-	//@Bean
-	public ClientConfig hazelCastConfig()
+	@Bean
+	public Config config()
 	{
-		ClientConfig clientConfig = new ClientConfig();
-		return clientConfig;
-		//Hazelcast.newHazelcastInstance(cfg);
+		NetworkConfig networkConfig = new NetworkConfig();
+		networkConfig.setPublicAddress("192.168.1.157").addOutboundPort(9701);
+		networkConfig.setPublicAddress("192.168.1.157").addOutboundPort(9702);
+		Config config = new Config();
+		config.setNetworkConfig(networkConfig);
+		return config;
 	}
 }
